ed73ee99ade73f434d9af6564d219ae967fea21c
EC2 Default User <ec2-user@ip-172-31-5-206.ap-southeast-1.compute.internal>
2018-12-31 14:03:57 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost:27017/yelp_camp_v10",{useNewUrlParser: true});
-mongoose.connect("mongodb://Box621863:box621863@ds145694.mlab.com:45694/yelpcamp",{useNewUrlParser: true});
+
+mongoose.connect(process.env.DATABASEURL,{useNewUrlParser: true});
+// mongoose.connect("mongodb://Box621863:box621863@ds145694.mlab.com:45694/yelpcamp",{useNewUrlParser: true});
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
